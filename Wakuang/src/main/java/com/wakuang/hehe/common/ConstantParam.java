8b192a50baff27f3f485614c40e949102f94f3a5
황원국 <@N15317_W>
2017-06-16 16:52:10 +0900

    수수료 유형, 금액유형 CONSTANT

--- Wakuang/src/main/java/com/wakuang/hehe/common/ConstantParam.java
+++ Wakuang/src/main/java/com/wakuang/hehe/common/ConstantParam.java
@@ -17,6 +17,19 @@ public class ConstantParam {
     public static final String COINTYPE_XRP      = "XRP";
 
     // 수수료유형
+    // 입금수수료, 인출수수료, 판매수수료, 구매수수료, 코인송금수수료,
+    public static final String CHARGE_DEPOSIT    = "DEPOSIT";
+    public static final String CHARGE_WITHDRAW   = "WITHDRAW";
+    public static final String CHARGE_SELL       = "SELL";
+    public static final String CHARGE_BUY        = "BUY";
+    public static final String CHARGE_REMITTANCE = "REMITTANCE";
 
     // 금액유형
+    public static final String COIN_INFO_PRICE   = "PRICE";
+    public static final String COIN_INFO_BUY     = "BUY";
+    public static final String COIN_INFO_SELL    = "SELL";
+    public static final String COIN_INFO_MAX     = "MAX";
+    public static final String COIN_INFO_MIN     = "MIN";
+    public static final String COIN_INFO_SUM     = "SUM";
+    public static final String COIN_INFO_VOLUME  = "VOLUME";
 }
