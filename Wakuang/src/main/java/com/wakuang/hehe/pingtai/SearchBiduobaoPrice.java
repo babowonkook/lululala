e07abc5008ecc0e4ecdae7330bade3d05d94ef39
황원국 <@N15317_W>
2017-06-16 16:59:48 +0900

    constant사용

--- Wakuang/src/main/java/com/wakuang/hehe/pingtai/SearchBiduobaoPrice.java
+++ Wakuang/src/main/java/com/wakuang/hehe/pingtai/SearchBiduobaoPrice.java
@@ -8,6 +8,7 @@ import java.util.Map.Entry;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.wakuang.hehe.common.ConstantParam;
 import com.wakuang.hehe.hanguo.util.SslTest;
 import com.wakuang.hehe.utils.WakuangStringUtils;
 
@@ -43,31 +44,31 @@ public class SearchBiduobaoPrice implements SearchPingtaiPrice {
             JsonNode values = node.getValue();
             ArrayNode datas = (ArrayNode) values;
             if (values.isArray()) {
-                Map coinInfo = new HashMap<String, BigDecimal>();
+                Map<String, BigDecimal> coinInfo = new HashMap<String, BigDecimal>();
                 int i = 0;
                 for (final JsonNode objNode : values) {
 
                     switch (i) {
                         case 1:
-                            coinInfo.put("PRICE", new BigDecimal(objNode.toString()));
+                            coinInfo.put(ConstantParam.COIN_INFO_PRICE, new BigDecimal(objNode.toString()));
                             break;
                         case 2:
-                            coinInfo.put("BUY", new BigDecimal(objNode.toString()));
+                            coinInfo.put(ConstantParam.COIN_INFO_BUY, new BigDecimal(objNode.toString()));
                             break;
                         case 3:
-                            coinInfo.put("SELL", new BigDecimal(objNode.toString()));
+                            coinInfo.put(ConstantParam.COIN_INFO_SELL, new BigDecimal(objNode.toString()));
                             break;
                         case 4:
-                            coinInfo.put("MAX", new BigDecimal(objNode.toString()));
+                            coinInfo.put(ConstantParam.COIN_INFO_MAX, new BigDecimal(objNode.toString()));
                             break;
                         case 5:
-                            coinInfo.put("MIN", new BigDecimal(objNode.toString()));
+                            coinInfo.put(ConstantParam.COIN_INFO_MIN, new BigDecimal(objNode.toString()));
                             break;
                         case 6:
-                            coinInfo.put("SUM", new BigDecimal(objNode.toString()));
+                            coinInfo.put(ConstantParam.COIN_INFO_SUM, new BigDecimal(objNode.toString()));
                             break;
                         case 7:
-                            coinInfo.put("VOLUME", new BigDecimal(objNode.toString()));
+                            coinInfo.put(ConstantParam.COIN_INFO_VOLUME, new BigDecimal(objNode.toString()));
                             break;
                         default:
                             break;
